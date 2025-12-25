80d4509c90178dcdd48681930c68350cceff59a5
Scott Gibb <smgibb@yahoo.com>
2025-12-25 00:20:56 +0000

    style: more issues
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import reactLogo from './assets/react.svg'
 import { invoke } from '@tauri-apps/api/core'
 import './App.css'
 
-function App () {
+function App() {
   const [greetMsg, setGreetMsg] = useState('')
   const [name, setName] = useState('')
 
-  async function greet () {
+  async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     setGreetMsg(await invoke('greet', { name }))
   }
 
 
 
-  
+
+
+
+
 
   return (
     <main className='container'>
