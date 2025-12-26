4c45942f0c2ae9ab5326649587ec4cf88597a125
Scott Gibb <smgibb@yahoo.com>
2025-12-26 20:46:17 +0000

    feat: add routes
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,9 +1,11 @@
 import { ReactNode } from "react";
 import { ToastContainer } from "react-toastify";
+import NavBar from "./navbar";
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col h-screen overflow-hidden">
+      <NavBar />
       <ToastContainer />
       <div>
         <div className="grow">{children}</div>
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,19 +1,43 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import React from "react";
 import { SettingsPage } from "./pages/settings";
+import { HomePage } from "./pages/home";
+import { SerialConfigurationPage } from "./pages/configuration/serial";
+import AboutPage from "./pages/about";
 
 type RouteType = {
   title: string;
   path: string;
   element: React.ReactNode;
   icon?: React.ReactNode;
+  isPrivate: boolean;
 };
 
 const allRoutes: RouteType[] = [
   {
-    title: "Settings",
+    title: "Home",
     path: "/",
+    isPrivate: false,
+    element: <HomePage />,
+  },
+  {
+    title: "Settings",
+    path: "/settings",
+    isPrivate: false,
+
     element: <SettingsPage />,
+  },
+  {
+    title: "About",
+    path: "/about",
+    isPrivate: false,
+    element: <AboutPage />,
+  },
+  {
+    title: "Serial Configuration",
+    path: "/configuration/serial",
+    isPrivate: true,
+    element: <SerialConfigurationPage />,
   },
 ];
 
