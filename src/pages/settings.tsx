75184167c5fa3e960c1117190f58f16d8f570b2d
Scott Gibb <smgibb@yahoo.com>
2025-12-26 19:54:38 +0000

    feat: add tailwind and daisyui
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,8 +1,18 @@
 /** Settings for the game.  This page is loaded once before a game starts to determine rules and player count. */
 
 import { useEffect } from "react";
+import { notifySuccess } from "../services/notifications";
 
 export function SettingsPage() {
   useEffect(() => {}, []);
-  return <button>Start Game</button>;
+  return (
+    <div>
+      <button
+        className="btn  btn-secondary"
+        onClick={() => notifySuccess("Settings saved!")}
+      >
+        Save Settings
+      </button>
+    </div>
+  );
 }
