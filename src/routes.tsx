b7fc481c32d3fb002ea24c5f5f73b109f0e1a5c9
Scott Gibb <smgibb@yahoo.com>
2025-12-27 12:02:04 +0000

    feat: add Navbar types
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route, useLocation } from "react-router-dom";
-import React from "react";
+import type { JSX, ReactNode } from "react";
+
 import { SettingsPage } from "./pages/settings";
 import { HomePage } from "./pages/home";
 import { SerialConfigurationPage } from "./pages/configuration/serial";
@@ -9,54 +10,56 @@ import { TcpConfigurationPage } from "./pages/configuration/tcp";
 type RouteType = {
   title: string;
   path: string;
-  element: React.ReactNode;
-  icon?: React.ReactNode;
-  isPrivate: boolean;
+  element: ReactNode;
+  icon?: ReactNode;
+  type: RouteTypeEnum;
 };
 
-const allRoutes: RouteType[] = [
+export enum RouteTypeEnum {
+  Configuration = "CONFIGURATION",
+  Control = "CONTROL",
+  General = "GENERAL",
+}
+
+export const routes: RouteType[] = [
   {
     title: "Home",
     path: "/",
-    isPrivate: false,
+    type: RouteTypeEnum.General,
     element: <HomePage />,
   },
   {
     title: "Settings",
     path: "/settings",
-    isPrivate: false,
-
+    type: RouteTypeEnum.Configuration,
     element: <SettingsPage />,
   },
   {
     title: "About",
     path: "/about",
-    isPrivate: false,
+    type: RouteTypeEnum.General,
     element: <AboutPage />,
   },
   {
     title: "Serial Configuration",
     path: "/configuration/serial",
-    isPrivate: true,
+    type: RouteTypeEnum.Configuration,
     element: <SerialConfigurationPage />,
   },
   {
     title: "TCP Configuration",
     path: "/configuration/tcp",
-    isPrivate: true,
+    type: RouteTypeEnum.Configuration,
     element: <TcpConfigurationPage />,
   },
 ];
 
-export const routes: RouteType[] = allRoutes;
-
-export default function AppRoutes(): React.ReactNode {
+export default function AppRoutes(): JSX.Element {
   const location = useLocation();
+
   return (
     <Routes location={location} key={location.pathname}>
-      {/* Map through the routes and create a Route for each */}
       {routes.map(({ path, element, title }) => (
-        // Wrap each route in an AnimationWrapper for animation effects
         <Route key={title} path={path} element={element} />
       ))}
     </Routes>
