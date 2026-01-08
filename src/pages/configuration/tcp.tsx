64c607917d59a29a1c9f82f980011accb706f5ab
Scott Gibb <smgibb@yahoo.com>
2026-01-08 22:02:50 +0000

    fix(ui): centering issues
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- src/pages/configuration/tcp.tsx
+++ src/pages/configuration/tcp.tsx
@@ -1,14 +1,14 @@
 /** Settings for the game.  This page is loaded once before a game starts to determine rules and player count. */
 
 import { useEffect } from "react";
-import { notifySuspense } from "../../services/notifications";
 import { NavLink } from "react-router-dom";
 
 export function TcpConfigurationPage() {
   useEffect(() => {}, []);
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="flex flex-col gap-4 w-full max-w-md">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-2xl font-bold mb-4">TCP Configuration</h1>
         <input
           type="string"
           placeholder="IP Address"
@@ -19,22 +19,16 @@ export function TcpConfigurationPage() {
           placeholder="Port"
           className="input input-bordered w-full"
         />
-        <div className="flex gap-2">
-          <button
-            className="btn btn-primary flex-1"
-            onClick={() => {
-              notifySuspense("Connecting...");
-            }}
-          >
-            Connect
-          </button>
-          <NavLink className="btn btn-secondary flex-1" to="/">
+        <div className="flex gap-2 flex-col">
+          <div className="flex gap-2 flex-row">
+            <button className="btn btn-primary">Connect</button>
+            <button className="btn btn-primary">Disconnect</button>
+          </div>
+          <button className="btn btn-secondary"> Continue</button>
+          <NavLink className="btn btn-secondary" to="/">
             Back
           </NavLink>
-          <NavLink
-            className="btn btn-secondary flex-1"
-            to="/control/master_control"
-          >
+          <NavLink className="btn btn-secondary" to="/control/master_control">
             Master Control
           </NavLink>
         </div>
