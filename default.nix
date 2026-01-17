151f406f55d5f39fecb8028126ed1b54f50c1ede
Scott Gibb <smgibb@yahoo.com>
2026-01-17 18:02:03 +0100

    APP COMPILES IN NIX BUT FAILS TO BUNDLE
    Signed-off-by: Scott Gibb <smgibb@yahoo.com>

--- default.nix
+++ default.nix
@@ -2,25 +2,33 @@
   lib,
   stdenv,
   rustPlatform,
-  fetchNpmDeps,
   cargo-tauri,
   glib-networking,
   openssl,
   pkg-config,
   webkitgtk_4_1,
   wrapGAppsHook4,
+
   bun,
   bun2nix,
+  nodejs,
+  typescript,
 }:
 
 let
   manifest = (lib.importTOML ./src-tauri/Cargo.toml).package;
 in
 rustPlatform.buildRustPackage (finalAttrs: {
-
   pname = manifest.name;
   version = manifest.version;
-  npmDeps = bun2nix.fetchBunDeps {
+
+  # 🔴 REQUIRED
+  src = ./.;
+
+  cargoHash = "sha256-0Lb7vCQoyasm9pNVbMb2dHI2F22T4tOr+xVgnULHuYI=";
+
+  # 🔴 Must be called bunDeps
+  bunDeps = bun2nix.fetchBunDeps {
     bunNix = import ./bun.nix;
     inherit (finalAttrs) src;
   };
@@ -29,29 +37,29 @@ rustPlatform.buildRustPackage (finalAttrs: {
   dontUseBunPatch = true;
   dontUseBunCheck = true;
 
-  cargoHash = "sha256-0Lb7vCQoyasm9pNVbMb2dHI2F22T4tOr+xVgnULHuYI=";
+  cargoRoot = "src-tauri";
+  buildAndTestSubdir = finalAttrs.cargoRoot;
 
   nativeBuildInputs = [
-    # Pull in our main hook
-    cargo-tauri.hook
-    # Make sure we can find our libraries
-    pkg-config
     bun
+    bun2nix.hook
+    cargo-tauri.hook
+    pkg-config
+    nodejs
+    typescript
   ]
-  ++ lib.optionals stdenv.hostPlatform.isLinux [ wrapGAppsHook4 ];
+  ++ lib.optionals stdenv.hostPlatform.isLinux [
+    wrapGAppsHook4
+  ];
 
   buildInputs = lib.optionals stdenv.hostPlatform.isLinux [
-    glib-networking # Most Tauri apps need networking
+    glib-networking
     openssl
     webkitgtk_4_1
   ];
 
-  # Set our Tauri source directory
-  cargoRoot = "src-tauri";
-  # And make sure we build there too
-  buildAndTestSubdir = finalAttrs.cargoRoot;
   meta = {
     description = "Led Strip Controller Tauri App";
-    mainProgram = "led-strip-controller-tauri-app";
+    mainProgram = manifest.name;
   };
 })
